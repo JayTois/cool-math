c5479a04c918f638af3f82ede7451d5fa1629dcf
JayTois <jtlizmi6@gmail.com>
2022-01-24 12:35:11 -0500

    Moved navigation bar file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopNavBarComponent } from './navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
