43283dbc36de858ec76d71adefa59d2dbea093e8
JayTois <jtlizmi6@gmail.com>
2022-02-22 12:31:10 -0500

    minor changes to lab 5

--- src/app/games/daily_list_items.ts
+++ src/app/games/daily_list_items.ts
@@ -1,17 +1,19 @@
-export const daily_items = [{
+export const daily_items = [
 
-img: "./assets/spot-the-difference.png",
-title: "Spot the Difference",
-description: "Those two photos are the same... right?"
+    {
 
-},
+        img: "./assets/spot-the-difference.png",
+        title: "Spot the Difference",
+        description: "Those two photos are the same... right?"
 
-{
+    },
 
-    img: "./assets/up-and-down-words.png",
-    title: "Up and Down Words",
-    description: "Solve the word pairs to complete the chain."
-    
+    {
+
+        img: "./assets/up-and-down-words.png",
+        title: "Up and Down Words",
+        description: "Solve the word pairs to complete the chain."
+
     },
 
 
